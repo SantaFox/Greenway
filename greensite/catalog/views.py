35215282aa4c24e14b84a1044808fe3c9bf11dad
SantaFox <santafox@gmail.com>
2021-03-31 00:26:09 +0300

    Small cleanup of views.py

--- greensite/catalog/views.py
+++ greensite/catalog/views.py
@@ -1,9 +1,8 @@
-from django.shortcuts import render
 from django.views import generic
 
-from .models import Product, ProductInfo
+from .models import Product
 
-# Create your views here.
+
 class DetailView(generic.DetailView):
     model = Product
     template_name = 'catalog/detail.html'
