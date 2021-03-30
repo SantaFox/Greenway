a28f9be38968d77a3ec33d5b32b859b299eb737a
Dmitry Blokhin <santafox@gmail.com>
2021-03-30 15:42:22 +0300

    Added simple details view

--- greensite/catalog/views.py
+++ greensite/catalog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views import generic
+
+from .models import Product, ProductInfo
 
 # Create your views here.
+class DetailView(generic.DetailView):
+    model = Product
+    template_name = 'catalog/detail.html'
