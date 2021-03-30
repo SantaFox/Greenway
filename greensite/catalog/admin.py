1b6189c8b860b6c365c9eca5a623549c4854db94
SantaFox <santafox@gmail.com>
2021-03-31 00:26:34 +0300

    Added some beauty to Admin part

--- greensite/catalog/admin.py
+++ greensite/catalog/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 
-from .models import Currency, Language, Product, ProductInfo, Tab
+from .models import Currency, Language, Product, ProductInfo, Tab, Price
+
+
+class LanguageAdmin(admin.ModelAdmin):
+    list_display = ('Code', 'Name')
+    list_editable = ['Name']
+    ordering = ['Code']
 
 
 class ProductInfoAdmin(admin.ModelAdmin):
@@ -8,6 +14,7 @@ class ProductInfoAdmin(admin.ModelAdmin):
     list_filter = ['Product', 'Language']
     list_editable = ['Name', 'Specification']
     ordering = ['Product__SKU', 'Language__Code']
+
 
 class TabAdmin(admin.ModelAdmin):
     # fieldsets = [
@@ -18,12 +25,20 @@ class TabAdmin(admin.ModelAdmin):
     list_display = ('Product', 'Language', 'Order', 'Name')
     list_filter = ['Product', 'Language']
     list_editable = ['Order', 'Name']
-    ordering = ['Product__SKU', 'Language__Code']
+    ordering = ['Product__SKU', 'Language__Code', 'Order']
     # search_fields = ['question_text']
 
 
+class PriceAdmin(admin.ModelAdmin):
+    list_display = ('Product', 'Currency', 'DateAdded', 'Price', 'PV')
+    list_filter = ['Product', 'Currency']
+    list_editable = ['DateAdded', 'Price', 'PV']
+    ordering = ['Product__SKU', 'Currency__Code', 'DateAdded']
+
+
 admin.site.register(Currency)
-admin.site.register(Language)
+admin.site.register(Language, LanguageAdmin)
 admin.site.register(Product)
 admin.site.register(ProductInfo, ProductInfoAdmin)
 admin.site.register(Tab, TabAdmin)
+admin.site.register(Price, PriceAdmin)
