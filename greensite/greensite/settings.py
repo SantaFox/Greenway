44dd5b27380f9ab121f3a8f234a8ccd2b4a34be8
SantaFox <santafox@gmail.com>
2021-03-31 23:19:22 +0300

    Added some security

--- greensite/greensite/settings.py
+++ greensite/greensite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 import os
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'bu3qv5!=8nvi%=2a4ad)fmv=)_a-jzj%vp27fw@(-)(8pds1t&'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
