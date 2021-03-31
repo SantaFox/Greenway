8eaac425697c656b9af2d3935dc886f9853561e8
Dmitry Blokhin <santafox@gmail.com>
2021-03-31 11:42:07 +0300

    Changed Details view to refer by ID or SKU

--- greensite/catalog/views.py
+++ greensite/catalog/views.py
@@ -1,8 +1,19 @@
 from django.views import generic
+from django.shortcuts import get_object_or_404, render
 
 from .models import Product
 
 
-class DetailView(generic.DetailView):
-    model = Product
-    template_name = 'catalog/detail.html'
+# We have to comment it because generic.DetailView cannot process different
+# identifiers from URL
+# class DetailView(generic.DetailView):
+#     model = Product
+#     template_name = 'catalog/detail.html'
+
+def detail(request, productid=None, sku=None):
+    if productid:
+        product = get_object_or_404(Product, pk=productid)
+    elif sku:
+        product = get_object_or_404(Product, SKU=sku)
+
+    return render(request, 'catalog/detail.html', {'product': product})
