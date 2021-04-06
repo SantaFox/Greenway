b1ce83c6bd119a4b1112889649793474ee077e78
SantaFox <santafox@gmail.com>
2021-04-06 22:59:48 +0300

    Added Counterparty admin model

--- greensite/ledger/admin.py
+++ greensite/ledger/admin.py
@@ -2,7 +2,16 @@ from django.contrib import admin
 
 from .models import Account, Counterparty, Order, OrderPosition
 
+
+class CounterpartyAdmin(admin.ModelAdmin):
+    list_display = ('User', 'Name', 'Phone', 'IsSupplier', 'IsCustomer')
+    # list_filter = ['Language', 'Product__Category', 'Product']
+    list_editable = ['Name', 'Phone', 'IsSupplier', 'IsCustomer']
+    ordering = ['User', 'Name']
+    search_fields = ['Name']
+
+
 admin.site.register(Account)
-admin.site.register(Counterparty)
+admin.site.register(Counterparty, CounterpartyAdmin)
 admin.site.register(Order)
 admin.site.register(OrderPosition)
