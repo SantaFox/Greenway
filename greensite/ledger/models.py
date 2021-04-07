7256dbee84cbd2089f54862ff67792c93f705b6b
SantaFox <santafox@gmail.com>
2021-04-07 23:28:24 +0300

    Updated "Ledger" models to have Operations as a main element

--- greensite/ledger/models.py
+++ greensite/ledger/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
 from products.models import Currency, Product
 
@@ -35,7 +36,7 @@ class Counterparty(models.Model):
     TimestampModified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.User} / {self.Name}'
+        return f'{self.Name}'
 
     class Meta:
         verbose_name_plural = "Counterparties"
@@ -44,39 +45,37 @@ class Counterparty(models.Model):
         ]
 
 
-class AtomCashOperation(models.Model):
-    TimestampCreated = models.DateTimeField(auto_now_add=True)
-    TimestampModified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.pk)
-
-
-class AtomProductOperation(models.Model):
-    TimestampCreated = models.DateTimeField(auto_now_add=True)
-    TimestampModified = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return str(self.pk)
-
-
-class Order(models.Model):
+class Operation(models.Model):
     User = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    DateOperation = models.DateField(blank=False)
+    Type = models.IntegerField(choices=[
+        (1, _('Customer Order')),
+        (2, _('Supply Order')),
+        (3, _('Received Payment')),
+        (4, _('Sent Payment')),
+        (5, _('Delivered Goods')),
+        (6, _('Received Goods')),
+        (7, _('Deposited Money')),
+        (8, _('Withdrawn money')),
+    ], blank=False, null=True)
     Counterparty = models.ForeignKey(Counterparty, on_delete=models.PROTECT)
-    DateCreated = models.DateField(blank=False)      #
+
+    # Delivery-related block
     DatePlaced = models.DateField(blank=True, null=True)            # Date placed with supplier /
     DateDispatched = models.DateField(blank=True, null=True)        # Date dispatched by supplier / to customer
     DateDelivered = models.DateField(blank=True, null=True)         # Date received from supplier / by customer
     TrackingNumber = models.CharField(max_length=50, blank=True)
+
+    # System block
     TimestampCreated = models.DateTimeField(auto_now_add=True)
     TimestampModified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.User} / {self.Counterparty} / {self.DateCreated}'
+        return f'{self.User} / {self.DateOperation } / {self.Counterparty} / {self.get_Type_display()}'
 
 
-class OrderPosition(models.Model):
-    Order = models.ForeignKey(Order, on_delete=models.PROTECT)
+class OperationPosition(models.Model):
+    Operation = models.ForeignKey(Operation, on_delete=models.PROTECT)
     Product = models.ForeignKey(Product, on_delete=models.PROTECT)
     Quantity = models.PositiveIntegerField(blank=False)
     Price = models.DecimalField(max_digits=10, decimal_places=2, blank=False)
@@ -85,9 +84,18 @@ class OrderPosition(models.Model):
     TimestampModified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return f'{self.Order} / {self.Product} / {self.Quantity} / {self.Price} / {self.Currency}'
+        return f'{self.Operation} / {self.Product} / {self.Quantity} / {self.Price} / {self.Currency}'
 
     # class Meta:
     #     constraints = [
     #         models.UniqueConstraint(fields=['Code'], name='unique_Order')
     #     ]
+
+
+class OperationAtom(models.Model):
+    Operation = models.ForeignKey(Operation, on_delete=models.PROTECT)
+    TimestampCreated = models.DateTimeField(auto_now_add=True)
+    TimestampModified = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.pk)
