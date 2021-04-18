e0cc9aae0e74a7476a20319de78704bd958ddd02
Dmitry Blokhin <santafox@gmail.com>
2021-04-18 19:17:26 +0300

    Updated ALLOWED_HOSTS

--- greensite/greensite/settings.py
+++ greensite/greensite/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mygreenway.team', '127.0.0.1',]
 
 INTERNAL_IPS = ['127.0.0.1',]
 
