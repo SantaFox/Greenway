90c50554e4c8f74efb2cc747b1915451370d6391
Dmitry Blokhin <santafox@gmail.com>
2021-04-18 14:23:37 +0300

    Added new fields to Admin sections for GreenwayUser

--- greensite/users/admin.py
+++ greensite/users/admin.py
@@ -3,4 +3,16 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import GreenwayUser
 
-admin.site.register(GreenwayUser, UserAdmin)
+
+class GreenwayUserAdmin(UserAdmin):
+
+    def __init__(self, *args, **kwargs):
+        super(UserAdmin, self).__init__(*args, **kwargs)
+
+        UserAdmin.list_display = list(UserAdmin.list_display) + ['ManagingPartner', 'ReferralCode']
+        UserAdmin.fieldsets = UserAdmin.fieldsets + (
+            ('Greenway-Specific Data', {'fields': ('ManagingPartner', 'ReferralCode',)}),
+        )
+
+
+admin.site.register(GreenwayUser, GreenwayUserAdmin)
