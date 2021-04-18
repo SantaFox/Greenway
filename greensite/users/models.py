2486b5194b1db4c8872b4f50ee65338a66b5dfc1
Dmitry Blokhin <santafox@gmail.com>
2021-04-18 14:23:05 +0300

    Added "Referral Code" to GreenwayUSer

--- greensite/users/models.py
+++ greensite/users/models.py
@@ -4,3 +4,4 @@ from django.db import models
 
 class GreenwayUser(AbstractUser):
     ManagingPartner = models.ForeignKey('self', on_delete=models.PROTECT, blank=True, null=True)
+    ReferralCode = models.CharField(max_length=50, blank=True)
