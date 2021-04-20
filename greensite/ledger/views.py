c7f6e1e2b6e12a682f0d25c70b1051b7c3926e85
Dmitry Blokhin <santafox@gmail.com>
2021-04-20 10:50:50 +0300

    Added @prepare_languages decorator in Ledger. TODO: move it to separate class

--- greensite/ledger/views.py
+++ greensite/ledger/views.py
@@ -1,5 +1,8 @@
+from functools import wraps
+
 from django.shortcuts import render
 from django.conf import settings
+from django.template.response import TemplateResponse
 
 from django_tables2 import RequestConfig
 
@@ -8,87 +11,68 @@ from .models import Account, Counterparty, Operation
 from .tables import AccountsTable, CounterpartyTable, CustomerOrdersTable
 
 
+def prepare_languages(view_func):
+    """
+    Decorator that language information
+    """
+
+    @wraps(view_func)
+    def _wrapped_view_func(request, *args, **kwargs):
+
+        cookie_lang = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME, settings.LANGUAGE_CODE)
+        if cookie_lang in ('en', 'el', 'ru'):
+            detail_lang = cookie_lang
+        else:
+            detail_lang = settings.LANGUAGE_CODE
+        language = Language.objects.get(Code=detail_lang)
+        languages = Language.objects.all().order_by('Code')
+
+        response = view_func(request, *args, **kwargs)
+
+        if type(response) == TemplateResponse:
+            if not response.context_data:
+                response.context_data = {}
+            response.context_data['language'] = language
+            response.context_data['languages'] = languages
+            return response.render()
+        else:
+            raise TypeError(
+                'Decorator \'@prepare_languages\' expects django.template.response.TemplateResponse as a view '
+                'returned value')
+
+    return _wrapped_view_func
+
+
+@prepare_languages
 def view_index(request):
-    # Work with selected language
-    cookie_lang = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME, settings.LANGUAGE_CODE)
-
-    if cookie_lang in ('en', 'el', 'ru'):
-        detail_lang = cookie_lang
-    else:
-        detail_lang = settings.LANGUAGE_CODE
-
-    language = Language.objects.get(Code=detail_lang)
-    languages = Language.objects.all().order_by('Code')
-
-    response = render(request, 'ledger/index.html', {
-        'language': language,
-        'languages': languages,
-    })
-
-    return response
+    return TemplateResponse(request, 'ledger/index.html')
 
 
+@prepare_languages
 def table_counterparties(request):
-    # Work with selected language
-    cookie_lang = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME, settings.LANGUAGE_CODE)
-
-    if cookie_lang in ('en', 'el', 'ru'):
-        detail_lang = cookie_lang
-    else:
-        detail_lang = settings.LANGUAGE_CODE
-
-    language = Language.objects.get(Code=detail_lang)
-    languages = Language.objects.all().order_by('Code')
-
-    table = CounterpartyTable(Counterparty.objects.all())
+    table = CounterpartyTable(Counterparty.objects.filter(User=request.user))
     RequestConfig(request).configure(table)
 
-    return render (request, 'ledger/table_counterparties.html', {
-        'language': language,
-        'languages': languages,
+    return TemplateResponse(request, 'ledger/table_counterparties.html', {
         'table': table,
     })
 
 
+@prepare_languages
 def table_accounts(request):
-    # Work with selected language
-    cookie_lang = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME, settings.LANGUAGE_CODE)
-
-    if cookie_lang in ('en', 'el', 'ru'):
-        detail_lang = cookie_lang
-    else:
-        detail_lang = settings.LANGUAGE_CODE
-
-    language = Language.objects.get(Code=detail_lang)
-    languages = Language.objects.all().order_by('Code')
-
-    table = AccountsTable(Account.objects.all())
+    table = AccountsTable(Account.objects.filter(User=request.user))
     RequestConfig(request).configure(table)
 
-    return render (request, 'ledger/table_accounts.html', {
-        'language': language,
-        'languages': languages,
+    return TemplateResponse(request, 'ledger/table_accounts.html', {
         'table': table,
     })
 
 
+@prepare_languages
 def table_customer_orders(request):
-    # Work with selected language
-    cookie_lang = request.COOKIES.get(settings.LANGUAGE_COOKIE_NAME, settings.LANGUAGE_CODE)
-
-    if cookie_lang in ('en', 'el', 'ru'):
-        detail_lang = cookie_lang
-    else:
-        detail_lang = settings.LANGUAGE_CODE
-
-    language = Language.objects.get(Code=detail_lang)
-    languages = Language.objects.all().order_by('Code')
-
-    table = CustomerOrdersTable(Operation.objects.filter(Type=1))
+    table = CustomerOrdersTable(Operation.objects.filter(User=request.user, Type=1))
     RequestConfig(request).configure(table)
 
-    return render (request, 'ledger/table_customer_orders.html', {
-        'language': language,
-        'languages': languages,
+    return TemplateResponse(request, 'ledger/table_customer_orders.html', {
         'table': table,
     })
