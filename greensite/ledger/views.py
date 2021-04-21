5fee970ca3d4642d77de0594b388ea3e4efbe659
Dmitry Blokhin <santafox@gmail.com>
2021-04-21 09:21:28 +0300

    Added permission checks on Ledger.views

--- greensite/ledger/views.py
+++ greensite/ledger/views.py
@@ -1,3 +1,5 @@
+from django.contrib.auth.decorators import login_required, permission_required
+from django.shortcuts import get_object_or_404
 from django.template.response import TemplateResponse
 
 from django_tables2 import RequestConfig
@@ -8,11 +10,14 @@ from .models import Account, Counterparty, Operation
 from .tables import AccountsTable, CounterpartyTable, CustomerOrdersTable
 
 
+@login_required
 @prepare_languages
 def view_index(request):
     return TemplateResponse(request, 'ledger/index.html')
 
 
+@login_required
+@permission_required('ledger.view_counterparty', raise_exception=True)
 @prepare_languages
 def table_counterparties(request):
     table = CounterpartyTable(Counterparty.objects.filter(User=request.user))
@@ -23,6 +28,22 @@ def table_counterparties(request):
     })
 
 
+@login_required
+@permission_required('ledger.edit_account', raise_exception=True)
+@prepare_languages
+def edit_counterparty(request):
+    return TemplateResponse(request, 'ledger/edit_counterparty.html', )
+
+
+@login_required
+@permission_required('ledger.delete_account', raise_exception=True)
+@prepare_languages
+def confirm_delete_counterparty(request):
+    return TemplateResponse(request, 'ledger/delete_counterparty.html', )
+
+
+@login_required
+@permission_required('ledger.view_account', raise_exception=True)
 @prepare_languages
 def table_accounts(request):
     table = AccountsTable(Account.objects.filter(User=request.user))
@@ -33,6 +54,25 @@ def table_accounts(request):
     })
 
 
+@login_required
+@permission_required('ledger.edit_account', raise_exception=True)
+@prepare_languages
+def edit_account(request, pk):
+    return TemplateResponse(request, 'ledger/edit_account.html', )
+
+
+@login_required
+@permission_required('ledger.delete_account', raise_exception=True)
+@prepare_languages
+def confirm_delete_account(request, pk):
+    account_instance = get_object_or_404(Account, pk)
+    return TemplateResponse(request, 'ledger/delete_account.html', {
+        'account': account_instance,
+    })
+
+
+@login_required
+@permission_required('ledger.view_operation', raise_exception=True)
 @prepare_languages
 def table_customer_orders(request):
     table = CustomerOrdersTable(Operation.objects.filter(User=request.user, Type=1))
