92c54f642a12c8e18d3d96c44bf096d64c8d0933
Dmitry Blokhin <santafox@gmail.com>
2021-04-23 13:12:59 +0300

    Moved CRUD processing of checkboxes to crud.js

--- greensite/ledger/static/ledger/crud.js
+++ greensite/ledger/static/ledger/crud.js
@@ -1,4 +1,25 @@
 $(document).ready(function(){
+    // Select/Deselect checkboxes
+    var checkbox = $('table tbody input[type="checkbox"]');
+    $("#selectAll").click(function(){
+        if(this.checked){
+            checkbox.each(function(){
+                this.checked = true;
+            });
+        } else{
+            checkbox.each(function(){
+                this.checked = false;
+            });
+        }
+    });
+    checkbox.click(function(){
+        if(!this.checked){
+            $("#selectAll").prop("checked", false);
+        }
+    });
+});
+
+$(document).ready(function(){
     $('.modal form').on('submit', function (e) {
         e.preventDefault();
         $.ajax({
