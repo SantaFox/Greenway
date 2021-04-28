2073c883bc9dcebb8d47c50d21d5566b8f972b84
Dmitry Blokhin <santafox@gmail.com>
2021-04-28 23:02:08 +0300

    Reformatted urls.py to better see CRUD modules

--- greensite/ledger/urls.py
+++ greensite/ledger/urls.py
@@ -5,10 +5,16 @@ from . import views
 app_name = 'ledger'
 urlpatterns = [
     path('', views.view_index, name='index'),
+
     path('accounts/', views.table_accounts, name='accounts'),
     path('accounts/action/', views.account_action, name='account_action'),
+    path('accounts/delete/', views.account_delete, name='account_delete'),
+
     path('counterparties/', views.table_counterparties, name='counterparties'),
     path('counterparties/action/', views.counterparty_action, name='counterparty_action'),
     path('counterparties/delete/', views.counterparty_delete, name='counterparty_delete'),
+
     path('customer_orders/', views.table_customer_orders, name='customer_orders'),
+    # path('customer_orders/action/', views.customer_orders_action, name='customer_orders_action'),
+    # path('customer_orders/delete/', views.customer_orders_delete, name='customer_orders_delete'),
 ]
