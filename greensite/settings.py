3306afccbc07e2f7a41b6ce9181ea388b5e0174b
Dmitry Blokhin <santafox@gmail.com>
2021-05-09 00:44:58 +0300

    Added mac-mini.santafox.local for local shared editing

--- greensite/settings.py
+++ greensite/settings.py
@@ -18,7 +18,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['mygreenway.team', '127.0.0.1',]
+ALLOWED_HOSTS = ['mygreenway.team', '127.0.0.1', 'mac-mini.santafox.local', ]
 
 INTERNAL_IPS = ['127.0.0.1',]
 
