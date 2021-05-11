d9d528b7b3829861e84428d8add911ac1e79362c
Dmitry Blokhin <santafox@gmail.com>
2021-05-11 21:15:30 +0300

    Updated logic for favicon.ico so it may be served from the root directory

--- greensite/urls.py
+++ greensite/urls.py
@@ -1,7 +1,10 @@
-from django.contrib import admin
-from django.urls import include, path, re_path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib import admin
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
+
 
 from . import views
 
@@ -13,6 +16,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     re_path(r'^markdownx/', include('markdownx.urls')),
     re_path('djga/', include('google_analytics.urls')),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ]
 
 if settings.DEBUG:
