50d2b137cbf0ac03aab384104516d9642bbba727
Dmitry Blokhin <santafox@gmail.com>
2021-05-17 07:47:34 +0300

    Some further improvements in Showcase template

--- showcase/views.py
+++ showcase/views.py
@@ -6,6 +6,19 @@ from .models import Carousel, CarouselInfo, Featurette, FeaturetteInfo
 
 @prepare_languages
 def view_index(request):
+    carousels = Carousel.objects.filter(Active=True).order_by('Order')
+
+    dict_carousel_infos = {ci.Carousel: ci for ci in
+                          CarouselInfo.objects.filter(Carousel__Active=True, Language=request.language_instance)}
+
+    carousel_set = []
+    for caro in carousels:
+        carousel_set.append(
+            dict(carousel=caro,
+                 carousel_info=dict_carousel_infos.get(caro),
+                 )
+        )
+
     featurettes = Featurette.objects.filter(Active=True).order_by('Order')
 
     dict_feat_infos = {fi.Featurette: fi for fi in
@@ -20,6 +33,7 @@ def view_index(request):
         )
 
     return TemplateResponse(request, 'showcase/index.html', {
+        'carousels': carousel_set,
         'featurettes': feat_set,
     })
 
