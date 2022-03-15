d0686bd3bc33a4ab3dfdb7357f3296ecd76288e5
Dmitry Blokhin <santafox@gmail.com>
2022-03-15 07:27:52 +0200

    update 6 - started to work on recalculations

--- ledger/static/ledger/order.init.js
+++ ledger/static/ledger/order.init.js
@@ -1,11 +1,10 @@
+// Function to highlight part of the string before first semicolon(:) char
 function formatProduct (product) {
   if (!product.id) {
     return product.text;
   }
 
-  var $product = $(
-    '<span><strong></strong><span></span></span>'
-  );
+  var $product = $('<span><strong></strong><span></span></span>');
 
   // Use .text() instead of HTML string concatenation to avoid script injection issues
   var str = product.text;
@@ -15,31 +14,33 @@ function formatProduct (product) {
   return $product;
 };
 
-// Select2
+// Initialize Select2 elements
 $("select.select2[name='Customer']").select2();
 $("select.select2[name='Currency']").select2();
 $("div.position-form select.select2[name$='Product']").select2({
     templateSelection: formatProduct,
     ajax: {
         data: function (params) {
+            // extra handler to pass order's currency to backend so only required prices/discounts
+            // will be returned. Empty Currency field should be processed on backend side
             var currencyData = $('select#id_Currency').select2('data');
             var query = {
                 q: params.term,
                 cur: currencyData[0].text
             }
-
-            // Query parameters will be ?q=[term]&type=public
             return query;
         }
     }
 }).on("select2:select", function(event) {
     var productData = event.params.data;
     var priceElement = $(event.target).closest("div.row").find("input[name$='Price']");
-
+    // Update Price input in the same row after new selection
     priceElement.val(productData.price);
 });
 
+// Initialize FlatPickr elements
 $("input.dateinput").flatpickr();
+
 $("div.position-form").formset({
     prefix: "customerorderposition_set",
     formTemplate: "div#empty_form",
@@ -59,6 +60,18 @@ $("div.position-form").formset({
             templateSelection: formatProduct
         });
     }
+});
+
+$(".position-form").find("input[type='number']").on("input", function() {
+    var parentRow = $(event.target).closest("div.row");
+    var elPrice = $(parentRow).find("input[name$='-Price']");
+    var elQty = $(parentRow).find("input[name$='-Quantity']");
+    var elDiscount = $(parentRow).find("input[name$='-Discount']");
+    var elSubtotal = $(parentRow).find("input[name$='-subtotal']");
+
+    const subtotal = (+elPrice * +elQty) - +elDiscount;
+    //const sum = $amount2.get().reduce((sum, el) => sum + +el.value, 0);
+    elSubtotal.text(subtotal);
 });
 
 
